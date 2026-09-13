d34bf9b8406050ca7a7d6e43e88601020fef3e5c
rosalia <hamundjarosalia9@gmail.com>
2026-09-13 14:11:32 +0200

    Week 1 - Practical Exercise 2

--- Week1/main.c
+++ Week1/main.c
@@ -2,8 +2,26 @@
 
 int main()
 {
-    printf("Municipal Financial Management System\n");
-    printf("Welcome to Windhoek Municipality\n");
+    char municipality[50];
+    char mayor[50];
+    int population;
+
+    printf("Municipal Financial Management System\n\n");
+
+    printf("Enter Municipality Name: ");
+scanf(" %49[^\n]", municipality);
+
+
+    printf("Enter Mayor: ");
+    scanf(" %49[^\n]", mayor);
+
+    printf("Enter Population: ");
+    scanf("%d", &population);
+
+    printf("\n---------------------------------\n");
+    printf("Municipality : %s\n", municipality);
+    printf("Mayor        : %s\n", mayor);
+    printf("Population   : %d\n", population);
 
     return 0;
 }
